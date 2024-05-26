33cef822b4aa68e432da467aa621a44b7ebe357f
Denis Pika <denirix@mail.ru>
2024-05-26 07:10:28 +0300

    Lessons and profile settings

--- schoolapp/forms.py
+++ schoolapp/forms.py
@@ -5,10 +5,17 @@ from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.db import models
 from django.core.validators import EmailValidator
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from phonenumbers import parse, is_valid_number, format_number, PhoneNumberFormat
+from datetime import date
+import phonenumbers
+
+phone_validator = RegexValidator(r'^\+?\d{1,20}$', 'Пожалуйста, введите действительный номер телефона.')
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, validators=[EmailValidator()])
-    phone = forms.CharField(max_length=20, required=True)
+    phone = forms.CharField(max_length=20, required=True, validators=[phone_validator])
     gender = forms.ChoiceField(choices=[('M', 'Мужской'), ('F', 'Женский')], required=True)
     birth_date = forms.DateField(required=False)
 
@@ -39,3 +46,35 @@ class CustomUserCreationForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('Этот email уже зарегистрирован')
         return email
+
+    def clean_username(self):
+         username = self.cleaned_data.get('username')
+         if User.objects.filter(username=username).exists():
+             raise forms.ValidationError('Этот username уже занят.')
+         return username
+
+    def clean_phone(self):
+       phone = self.cleaned_data.get('phone')
+       try:
+           parsed_phone = parse(phone, None)
+           if not is_valid_number(parsed_phone):
+               raise forms.ValidationError('Некорректный номер телефона.')
+           formatted_phone = format_number(parsed_phone, PhoneNumberFormat.INTERNATIONAL)
+           return formatted_phone
+       except phonenumbers.phonenumberutil.NumberParseException:
+           raise forms.ValidationError('Некорректный номер телефона.')
+
+    def clean_birth_date(self):
+        birth_date = self.cleaned_data.get('birth_date')
+        if not birth_date:
+            return birth_date
+
+        min_date = date(2006, 1, 1)
+        max_date = date.today()
+
+        if birth_date >= max_date:
+            raise ValidationError('Дата рождения не может быть позже сегодняшней даты.')
+        if birth_date >= min_date:
+            raise ValidationError('Вы должны быть старше 18 лет.')
+
+        return birth_date
